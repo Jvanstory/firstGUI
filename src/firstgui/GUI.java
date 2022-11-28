24650268881cfbe3903cd795f01e019b0df360a5
Jvanstory <jvanstor@uncc.edu>
2022-11-27 21:28:57 -0500

    Initial commit

---
+++ src/firstgui/GUI.java
@@ -0,0 +1,57 @@
+
+package firstgui;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author joeva
+ */
+public class GUI implements ActionListener{
+    
+    private JFrame frame;
+    private JButton button;
+    private JLabel label;
+    private JPanel panel;
+    private int count = 0; 
+    
+    public GUI(){
+        frame = new JFrame();
+        
+        button = new JButton("Click Me");
+        button.addActionListener(this);
+        
+        label = new JLabel("Number of clicks: 0");
+        
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0,1));
+        panel.add(button);
+        panel.add(label);
+        
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("OUR GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
+    }
+    
+}
